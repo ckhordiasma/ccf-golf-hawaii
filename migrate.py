4a945c8992f4a1a5955a5ca6186f05cd08819dc6
Christopher Kodama <ckhordiasma@gmail.com>
2023-04-30 23:34:31 -0400

    updated with new header, misc
    updateed migrate script to new header format. also removed contents.html from git tree so that it won't ever appear on the main site

--- migrate.py
+++ migrate.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 import re
 
+sections = [
+    {'name': 'HOME', 'id': 'SITE_HEADER'},
+    {'name': 'SPONSORSHIP', 'id': 'sponsorship'},
+    {'name': 'DONATIONS', 'id': 'donations'},
+    {'name': 'SCHOLARSHIP/GRANT', 'id': 'scholarship-grant'},
+    {'name': '2022 ARCHIVE', 'id': '2022-archive'},
+    {'name': 'PHOTOS', 'id': 'photos'},
+    {'name': 'CONTACT US', 'id': 'SITE_FOOTER'},
+]
 
-
-sections = ['HOME', 'SPONSORS', 'DONATIONS', 'SCHOLARSHIP/GRANT', 'PHOTOS', 'CONTACT US']
-section_orders = [1, 2, 4, 5, 6]
-make_id = lambda x: 'SITE_FOOTER' if x == 'CONTACT US' else re.sub('[/ ]','-',x).lower()
 
 with open("contents.html", encoding='utf-8') as fp:
     soup = BeautifulSoup(fp, 'html.parser')
@@ -16,19 +21,19 @@ with open("contents.html", encoding='utf-8') as fp:
         navbar = elem.parent
         navbar.string = ''
         for section in sections:
-            new_link = soup.new_tag('a', href='#'+make_id(section))
-            new_link.string = section
+            new_link = soup.new_tag('a', href='#' + section['id'])
+            new_link.string = section['name']
             new_link['class'] = 'underline-on-hover'
             navbar.append(new_link)
-            navbar.append('\xa0\xa0\xa0\xa0\xa0')
+            navbar.append('\xa0\xa0\xa0\xa0')
     section_container = soup.findAll('div', {"data-mesh-id": "Containerc1dmpinlineContent-gridContainer"})[0]
     section_divs = section_container.findChildren("section", recursive=False)
     
-    section_divs[1].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = make_id(sections[0])
-    section_divs[2].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = make_id(sections[1])
-    section_divs[4].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = make_id(sections[2])
-    section_divs[5].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = make_id(sections[3])
-    section_divs[6].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = make_id(sections[4])
+    section_divs[2].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = sections[1]['id']
+    section_divs[3].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = sections[2]['id']
+    section_divs[4].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = sections[3]['id']
+    section_divs[5].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = sections[4]['id']
+    section_divs[6].findChildren("div", {"data-testid": "inline-content"})[0]["id"] = sections[5]['id']
     
 
     styles = soup.find(id="css_masterPage")
